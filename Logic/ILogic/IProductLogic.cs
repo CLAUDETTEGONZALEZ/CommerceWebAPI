2c00bcd966fb786ac1a674be514c5383809c3343
CLAUDETTEGONZALEZ <claudettegonzalezf5@gmail.com>
2023-01-27 12:20:25 +0100

    "Ruta Product"

--- Logic/ILogic/IProductLogic.cs
+++ Logic/ILogic/IProductLogic.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Entities
+namespace Logic.ILogic
 {
-    internal class UserItem
+    internal class IProductLogic
     {
     }
 }
---
+++ WebApplication1/IService/IProductService.cs
@@ -0,0 +1,6 @@
+﻿namespace APIService.IService
+{
+    public class IProductService
+    {
+    }
+}
